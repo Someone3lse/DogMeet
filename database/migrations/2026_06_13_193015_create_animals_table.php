2e2fc10ab68da088e07f242bcba3e139908167cf
Gabriel Kewryghan <g.rosas@aluno.senai.br>
2026-06-14 00:25:23 -0500

    Correções

--- database/migrations/2026_06_13_193015_create_animals_table.php
+++ database/migrations/2026_06_13_193015_create_animals_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('animals', function (Blueprint $table) {
-
             $table->id();
             $table->foreignId('user_id')
                 ->constrained()
